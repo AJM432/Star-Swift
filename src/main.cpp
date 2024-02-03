3750da0dce4c3f183f934ffa912de36562d77e8e
AJM432 <49791407+AJM432@users.noreply.github.com>
2024-02-03 17:37:40 -0500

    added kinetic energy plot

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
+#include "implot.h"
 
 #include <stdio.h>
 #include <random>
@@ -10,8 +11,8 @@
 
 #include "QuadTree.hpp"
 
-const int NUM_STARS = 5;
-const int RADIUS = 20;
+const int NUM_STARS = 5000;
+const int RADIUS = 200;
 
 int main( int argc, char* argv[] )
 {
@@ -31,13 +32,13 @@ int main( int argc, char* argv[] )
   SDL_GetCurrentDisplayMode(0, &DM);
   auto SCREEN_HEIGHT = DM.h-WINDOW_GAP;
   auto SCREEN_WIDTH = SCREEN_HEIGHT;
-  // auto SCREEN_WIDTH = DM.w;
+  auto ACTUAL_WIDTH = DM.w;
 
     SDL_Window* window = SDL_CreateWindow(
         "Galaxy Modeller",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH,
+        ACTUAL_WIDTH,
         SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN
     );
@@ -55,6 +56,7 @@ int main( int argc, char* argv[] )
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+    ImPlot::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
@@ -67,14 +69,22 @@ int main( int argc, char* argv[] )
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
 
-    // Our state
+    // State Variables
     ImVec4 galaxy_color = ImVec4(0.25f, 0.35f, 0.90f, 1.00f);
-    float gravity_strength = 100.f;
+    float gravity_strength = 100.f; // also the point mass
     float max_speed = 200.f;
-    float theta = 0.5f;
+    float theta = 1.0f;
     int soft_power = 2;
     bool show_velocity_vectors = false;
     bool show_gravity_vectors = false;
+    double total_kinetic_energy = 0;
+    double total_gravitational_potential_energy = 0;
+    const int plot_size = 1001;
+    static float kinetic_energies[plot_size];
+    static float xs1[plot_size];
+    for (int i = 0; i < plot_size; ++i) {
+        xs1[i] = i * 1.0f;
+    }
 
     SDL_Event e; 
     bool quit = false; 
@@ -117,8 +127,15 @@ int main( int argc, char* argv[] )
 
       root->update_galaxy(root, deltaTime);
 
+      // reset state variables
+      total_gravitational_potential_energy = 0;
+      total_kinetic_energy = 0;
       for (int i=0; i < NUM_STARS; i++) {
         Point *p = stars[i];
+        total_kinetic_energy += (1.0/2.0)*gravity_strength*(p->vx)*(p->vx); // 1/2mv^2
+        total_gravitational_potential_energy += (1.0/2.0)*gravity_strength*(p->vx)*(p->vx); // 1/2mv^2
+
+
         p->update_star_color(root->center_of_mass_x, root->center_of_mass_y, RADIUS, galaxy_color.x, galaxy_color.y, galaxy_color.z);
         SDL_SetRenderDrawColor(renderer, p->r, p->g, p->b, 255);
         // SDL_SetRenderDrawColor(renderer, galaxy_color.x*255, galaxy_color.y*255, galaxy_color.z*255, galaxy_color.z*255);
@@ -138,16 +155,40 @@ int main( int argc, char* argv[] )
 
       static int counter = 0;
 
-      ImGui::Begin("Settings");
-      ImGui::Text("Galaxy Modeller Parameters");
-      ImGui::SliderFloat("Gravity Strength", &gravity_strength, 0.0f, 10000.f);
+      ImGuiWindowFlags window_flags = 0;
+      window_flags |= ImGuiWindowFlags_NoBackground;
+      bool p_open = true;
+
+      ImGui::Begin("Galaxy Settings", &p_open, window_flags);
+      ImGui::SeparatorText("General");
+      ImGui::SliderFloat("Gravitational Strength", &gravity_strength, 0.0f, 10000.f);
       ImGui::SliderFloat("Max Star Velocity", &max_speed, 0.0f, 10000.f);
       ImGui::SliderFloat("Theta Threshold", &theta, 0.0f, 5.0f);
-      ImGui::SliderInt("Softening Power", &soft_power, -5, 5);
+      ImGui::SliderInt("Collision Softening", &soft_power, -5, 5);
       ImGui::ColorEdit3("Galaxy Color", (float*)&galaxy_color); // Edit 3 floats representing a color
+      ImGui::SeparatorText("Vector Display");
       ImGui::Checkbox("Show Velocity Vectors", &show_velocity_vectors);
       ImGui::Checkbox("Show Gravity Vectors", &show_gravity_vectors);
+      ImGui::SeparatorText("Stats");
 
+    
+    ////// create plot
+    // static double xs2[20], ys2[20];
+    // for (int i = 0; i < 20; ++i) {
+    //     xs2[i] = i * 1/19.0f;
+    //     ys2[i] = xs2[i] * xs2[i];
+    // }
+    for (int i = 1; i < plot_size; ++i) {
+        kinetic_energies[i-1] = kinetic_energies[i];
+    }
+    kinetic_energies[plot_size-1] = total_kinetic_energy;
+    if (ImPlot::BeginPlot("System Energy")) {
+        ImPlot::SetupAxes("time(ms)","Energy(J)");
+        ImPlot::PlotLine("Kinetic Energy", xs1, kinetic_energies, plot_size);
+        // ImPlot::PlotLine("Gravitational Potential Energy", xs2, ys2, 20);
+        ImPlot::EndPlot();
+    }
+      ////////////
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
       ImGui::End();
 
@@ -166,6 +207,7 @@ int main( int argc, char* argv[] )
   // Cleanup
   ImGui_ImplSDLRenderer2_Shutdown();
   ImGui_ImplSDL2_Shutdown();
+  ImPlot::DestroyContext();
   ImGui::DestroyContext();
 
   SDL_DestroyRenderer(renderer);
